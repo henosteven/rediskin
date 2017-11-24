4cbbe79abe268c5c54ff45f7b0af6a6f5cae59a8
jinjing <liujinjing@didichuxing.com>
2017-11-24 16:15:56 +0800

    完善正则例子

--- src/test/regexp.go
+++ src/test/regexp.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-    pat := `h.*?o`
+    pat := `h(.*?)o`
     re := regexp.MustCompile(pat)
     fmt.Println(reflect.TypeOf(re)) //*regexp.Regexp
     result := re.Find([]byte("hello heno steven"))
@@ -18,4 +18,13 @@ func main() {
     allResult := re.FindAll([]byte("hello heno steven"), -1)
     fmt.Println(allResult) //[[104 101 108 108 111] [104 101 110 111]]
     fmt.Println(reflect.TypeOf(allResult)) //[][]uint8
+
+    fmt.Println(re.FindAllString("hello heno steven", -1)) //[hello heno]
+    fmt.Println(re.FindStringIndex("i~hello heno steven")) //[2 7]
+    fmt.Println(re.FindAllStringIndex("i~hello heno steven", -1)) //[[2 7] [8 12]]
+    fmt.Println(re.FindIndex([]byte("i~hello heno steven"))) //[2 7]
+    fmt.Println(re.FindAllIndex([]byte("i~hello heno steven"), -1)) //[[2 7] [8 12]]
+
+    fmt.Println(re.FindSubmatch([]byte("i~hello heno steven"))) //[[104 101 108 108 111] [101 108 108]]
+    fmt.Println(re.FindStringSubmatch("i~hello heno steven")) //[hello ell]
 }
