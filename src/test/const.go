8f73f3e485eefc4488871407251b9d8ab24c08f6
henosteven <henosteven@126.com>
2017-11-27 15:16:20 +0800

    const v [type] = value  && iota

---
+++ src/test/const.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    const (
+        a = iota
+        b
+        c = "heno"
+        d //会延续上一个的操作
+        e = iota
+        f
+    )
+    //0 1 heno heno 4 5
+    fmt.Println(a, b, c, d, e, f)
+}
