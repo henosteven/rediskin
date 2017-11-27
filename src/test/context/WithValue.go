a57635577e9fd025d665bdca5ba6e0df73a2229a
henosteven <henosteven@126.com>
2017-11-27 13:38:45 +0800

    增加WithValue的参数interface{}部分说明

--- src/test/context/WithValue.go
+++ src/test/context/WithValue.go
@@ -11,6 +11,8 @@ var wg sync.WaitGroup
 
 func main() {
     ctx := context.Background()
+    //func WithValue(parent Context, key, val interface{}) Context
+    //注意这里的interface{}, key 和 val 都可以是任意类型的值
     cctx := context.WithValue(ctx, "key-user", "value-heno")
     wg.Add(1)
     go child1(cctx)
