798dd816daef5020d70ec84eef8eab2a1c869e25
henosteven <henosteven@126.com>
2017-12-02 23:08:19 +0800

    go new return *Type

---
+++ src/test/new.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+)
+
+type User struct {
+    Name string
+    Age int8
+}
+
+func main() {
+   u_1 := new(User) //返回指针
+   u_1.Name = "heno"
+   u_1.Age = 28
+
+   u_2 := User{} //返回值
+   u_2.Name = "heno"
+   u_2.Age = 28
+   
+   //&{heno 28} {heno 28}
+   fmt.Println(u_1, u_2)
+}
