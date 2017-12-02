42c81b62fee99e16ab805e6f6e9e6a4ff72015b9
henosteven <henosteven@126.com>
2017-12-02 15:24:14 +0800

    interface作为参数及操作者

---
+++ src/test/interfaceasargv.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Listener interface {
+    Accept()
+    ShowListenedList()
+}
+
+type HenoListener struct {
+    Listener
+}
+
+func (h HenoListener) ShowListenedList () {
+    fmt.Println("just show HenoListener")
+}
+
+func Job(l Listener) {
+    l.ShowListenedList()
+}
+
+func main() {
+    hl := HenoListener{}
+    Job(hl)
+}
---
+++ src/test/interfaceasobj.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+type Shape interface {
+    Area() interface{}
+    Display()  
+}
+
+type Rect struct {
+    X int
+    Y int
+}
+
+func (rect *Rect) Area() interface{} {
+    return rect.X * rect.Y
+}
+
+func (rect *Rect) Display() {
+    fmt.Println("x:", rect.X, "y:", rect.Y)
+}
+
+type Round struct {
+    R int
+}
+
+func (round *Round) Area() interface{} {
+    return round.R * round.R * int(math.Pi)
+}
+
+func (round *Round) Display() {
+    fmt.Println("R:", round.R)
+}
+
+func Show(shape Shape) {
+    fmt.Println("area", shape.Area())
+    shape.Display()
+}
+
+func main() {
+    //Rect does not implement Shape (Area method has pointer receiver)
+    //这里主要说明 *SomeType 实现了接口，并不代表 SomeType也实现了接口
+    //所以需要使用接口类型的话，就需要使用&VarSomeType
+    //rect := Rect{3, 5}
+
+    rect := &Rect{3, 5}
+    Show(rect);
+    round := &Round{3}
+    Show(round);
+}
