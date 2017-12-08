960c1d4b3caad859447f8396e007529adfbfb8de
henosteven <henosteven@126.com>
2017-12-08 11:22:53 +0800

    reflect.ValueOf(xxx).IsNil() 和 IsValid()的使用 前者判断是否为空，后者判断是否为Zero value

--- src/test/reflect/structmethod.go
+++ src/test/reflect/structmethod.go
@@ -19,6 +19,10 @@ func(t T) Add(i , j int) int{
     return i + j
 }
 
+type User interface {
+
+}
+
 func main() {
     var t T
 
@@ -33,5 +37,55 @@ func main() {
     result := reflect.ValueOf(t).MethodByName("Add").Call([]reflect.Value{reflect.ValueOf(i), reflect.ValueOf(j)})
     fmt.Println(result) //[<int Value>] 是一个slice
     fmt.Println(result[0].Int()) //3 
-}
 
+    var strmap map[string]string
+    //output: strmap nil
+    if strmap == nil {
+        fmt.Println("strmap nil")
+    }
+    
+    strmapref := reflect.ValueOf(strmap)
+    //output: strmap nil
+    if strmapref.IsNil() {
+        fmt.Println("strmap nil")
+    }
+    
+    /*
+    IsNil reports whether its argument v is nil. The argument must be a chan, func, interface, map, pointer, or slice value; if it is not, IsNil panics. Note that IsNil is not always equivalent to a regular comparison with nil in Go. For example, if v was created by calling ValueOf with an uninitialized interface variable i, i==nil will be true but v.IsNil will panic as v will be the zero Value.
+    要求reflect.ValueOf的参数必须为chan func interface map pointer slice, 如果不是的就会panic
+    如果参数是一个为初始化的接口类型变量 i==nil为true, 但是v.IsNil, 因为reflect.ValueOf(v) 就不是上述类型了
+    */
+
+    var u User
+    //panic: reflect: call of reflect.Value.IsNil on zero Value
+    //u就是一个未初始化的一个接口类型变量
+    /*
+    if reflect.ValueOf(u).IsNil() {
+    }
+    */
+
+    strmap = make(map[string]string)
+    strmapref = reflect.ValueOf(strmap)
+    //output: strmap not nil
+    if strmapref.IsNil() {
+        fmt.Println("strmap nil")
+    } else {
+        fmt.Println("strmap not nil")
+    }
+
+    refm := reflect.ValueOf(t).MethodByName("heno")
+    fmt.Println(refm)
+    //panic: reflect: call of reflect.Value.IsNil on zero Value
+    /*
+    if refm.IsNil() {
+        fmt.Println("result is nil")
+    }
+    */
+    //output: refm is invalid
+    //IsValid reports whether v represents a value. It returns false if v is the zero Value. If IsValid returns false
+    if refm.IsValid() {
+        fmt.Println("refm is valid")
+    } else {
+        fmt.Println("refm is invalid")
+    }
+}
